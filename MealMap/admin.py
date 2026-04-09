0ce27f849139dc653e2934febd7192ba3a7c143b
KimMayoWakeTech <Kamayo@my.waketech.edu>
2026-04-09 16:07:49 -0400

    Add admin for Recipe, Ingredient, MealPlan models
    1.8 Register Recipe, Ingredient, MealPlan and MealPlanEntry in Django admin. Adds an IngredientInline for editing ingredients on recipe pages and configures admin classes with list_display, search_fields, list_filter and ordering to improve management of recipes and meal plans in the admin interface.

--- MealMap/admin.py
+++ MealMap/admin.py
@@ -1,3 +1,38 @@
 from django.contrib import admin
+from .models import Recipe, Ingredient, MealPlan, MealPlanEntry
 
-# Register your models here.
+
+class IngredientInline(admin.TabularInline):
+    model = Ingredient
+    extra = 1
+
+
+@admin.register(Recipe)
+class RecipeAdmin(admin.ModelAdmin):
+    list_display = ("title", "owner", "cook_time", "created_at")
+    search_fields = ("title", "description", "owner__username")
+    list_filter = ("created_at",)
+    ordering = ("title",)
+    inlines = [IngredientInline]
+
+
+@admin.register(Ingredient)
+class IngredientAdmin(admin.ModelAdmin):
+    list_display = ("name", "recipe", "quantity")
+    search_fields = ("name", "recipe__title")
+    list_filter = ("recipe",)
+
+
+@admin.register(MealPlan)
+class MealPlanAdmin(admin.ModelAdmin):
+    list_display = ("title", "owner", "week_start", "created_at")
+    search_fields = ("title", "owner__username")
+    list_filter = ("week_start", "created_at")
+    ordering = ("-week_start",)
+
+
+@admin.register(MealPlanEntry)
+class MealPlanEntryAdmin(admin.ModelAdmin):
+    list_display = ("meal_plan", "recipe", "day_of_week", "meal_type")
+    search_fields = ("meal_plan__title", "recipe__title")
+    list_filter = ("day_of_week", "meal_type")
