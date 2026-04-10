c42cc49238d74484e7da93ebb38bd65446d8fd46
KimMayoWakeTech <Kamayo@my.waketech.edu>
2026-04-10 10:48:00 -0400

    2.1 Add signup/login views, templates, and navbar
    Add user authentication flow: introduce SignUpView (UserCreationForm) and register a signup URL in MealMap.urls. Include Django auth URLs at /accounts/ and add login/signup templates that use crispy forms. Update navbar to use named URL tags, wire login/signup links, and replace logout link with a POST form. Add crispy_forms and crispy_bootstrap5 to INSTALLED_APPS, set CRISPY_TEMPLATE_PACK, and configure LOGIN_REDIRECT_URL and LOGOUT_REDIRECT_URL.

--- MealMap/views.py
+++ MealMap/views.py
@@ -1,5 +1,13 @@
-from django.views.generic import TemplateView
+from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse_lazy
+from django.views.generic import CreateView, TemplateView
 
 
 class HomePageView(TemplateView):
     template_name = "home.html"
+
+
+class SignUpView(CreateView):
+    form_class = UserCreationForm
+    template_name = "registration/signup.html"
+    success_url = reverse_lazy("login")
--- django_project/settings.py
+++ django_project/settings.py
@@ -38,6 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    'crispy_forms',
+    'crispy_bootstrap5',
+
+
     'MealMap',
 ]
 
@@ -126,6 +130,9 @@ STATICFILES_DIRS = [
 # Crispy forms
 CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
 CRISPY_TEMPLATE_PACK = "bootstrap5"
+
+LOGIN_REDIRECT_URL = "home"
+LOGOUT_REDIRECT_URL = "home"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
